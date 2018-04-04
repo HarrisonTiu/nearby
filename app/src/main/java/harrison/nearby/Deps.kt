38114a30c24db3a22ef6bbc0bdb33394767bf7ad
Harrison Tiu <harrison.7iu@gmail.com>
2018-04-04 18:52:14 +0800

    Add initial project

---
+++ app/src/main/java/harrison/nearby/Deps.kt
@@ -0,0 +1,15 @@
+package harrison.nearby
+
+/**
+ * Harrison Tiu
+ * RNG Studios, Inc.
+ *
+ * The class that will contain all dependency libraries in the app.
+ */
+
+class Deps {
+    companion object {
+        var BUTTERKNIFE_VERSION: String = ""
+        var BUTTERKNIFE: String = ""
+    }
+}
